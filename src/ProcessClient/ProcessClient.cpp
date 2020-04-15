072aa15ba47629087f341a3eef91f59d466605ae
Vasilios Magriplis <etameson@gmail.com>
2020-04-15 18:39:19 +1000

    Minor changes.

--- src/ProcessClient/ProcessClient.cpp
+++ src/ProcessClient/ProcessClient.cpp
@@ -1,6 +1,22 @@
+#include <Windows.h>
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello from client!" << std::endl;
+	TCHAR szName[] = TEXT("MyFileMappingObject");
+	HANDLE hMapFile = OpenFileMapping(
+		FILE_MAP_ALL_ACCESS,   // read/write access
+		true,                 // do not inherit the name
+		szName);               // name of mapping object
+	if (hMapFile)
+	{
+		std::cout << "Client opened handle!" << std::endl;
+		CloseHandle(hMapFile);
+	}
+	else
+	{
+		std::cout << "Client failed to open handle!" << std::endl;
+	}
+
+	std::cout << "Client exited!" << std::endl;
 }
--- src/ProcessServer/ProcessServer.cpp
+++ src/ProcessServer/ProcessServer.cpp
@@ -2,6 +2,8 @@
 #include <Windows.h>
 #include <string>
 
+#define BUF_SIZE 256
+
 int main(int argc, TCHAR* argv[])
 {
 	std::wstring buffer;
@@ -15,6 +17,29 @@ int main(int argc, TCHAR* argv[])
 	PROCESS_INFORMATION pi{ 0 };
 	si.cb = sizeof(si);
 
+	std::wstring filePath;
+	filePath.resize(250);
+	GetModuleFileName(nullptr, &filePath[0], filePath.size());
+	filePath.shrink_to_fit();
+	std::wcout << filePath << std::endl;
+
+	TCHAR szName[] = TEXT("MyFileMappingObject");
+	HANDLE hMapFile = CreateFileMapping(
+		INVALID_HANDLE_VALUE,    // use paging file
+		NULL,                    // default security
+		PAGE_READWRITE,          // read/write access
+		0,                       // maximum object size (high-order DWORD)
+		BUF_SIZE,                // maximum object size (low-order DWORD)
+		szName);                 // name of mapping object
+	if (hMapFile)
+	{
+		std::cout << "Server opened handle!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Server failed to open handle!" << std::endl;
+	}
+
 	// https://docs.microsoft.com/en-us/windows/win32/procthread/creating-processes
 	bool successfullyCreatedUpdateProcess =
 		CreateProcess(
@@ -22,7 +47,7 @@ int main(int argc, TCHAR* argv[])
 			nullptr,				// Command line
 			nullptr,				// Process handle not inheritable
 			nullptr,				// Thread handle not inheritable
-			false,					// Set handle inheritance to FALSE
+			true,					// Set handle inheritance to FALSE
 			0,						// No creation flags
 			nullptr,				// Use parent's environment block
 			nullptr,				// Use parent's starting directory 
@@ -40,6 +65,12 @@ int main(int argc, TCHAR* argv[])
 	else
 	{
 		std::wcout << "Failed." << GetLastError() << std::endl;
+	}
+
+	Sleep(10000);
+	if (hMapFile)
+	{
+		CloseHandle(hMapFile);
 	}
 
 	return 0;
