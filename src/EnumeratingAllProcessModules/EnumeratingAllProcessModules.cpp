a760d34df42eaa80f2077e4a0e35f375e4358959
Vasilios Magriplis <etameson@gmail.com>
2021-11-28 17:45:27 +1100

    Minor changes

--- src/EnumeratingAllProcessModules/EnumeratingAllProcessModules.cpp
+++ src/EnumeratingAllProcessModules/EnumeratingAllProcessModules.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
 #include <format>
 #include <string>
+#include <memory>
+#include <vector>
 #include <windows.h>
 #include <tchar.h>
 #include <stdio.h>
 #include <psapi.h>
+
+struct HandleDeleter final
+{
+    void operator()(HANDLE handle) { CloseHandle(handle); }
+};
+using HandleUniquePtr = std::unique_ptr<std::remove_pointer<HANDLE>::type, HandleDeleter>;
 
 // https://docs.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-enumprocessmodules
 int PrintModules(DWORD processID)
@@ -12,57 +20,54 @@ int PrintModules(DWORD processID)
     std::wcout << "Process ID: " << processID << std::endl;
 
     // Get a handle to the process.
-    HANDLE hProcess;
-    hProcess = OpenProcess(
+    HandleUniquePtr hProcess(OpenProcess(
         PROCESS_QUERY_INFORMATION | PROCESS_VM_READ,
         false, 
         processID
-    );
+    ));
     if (!hProcess)
         return 1;
 
-    // Get a list of all the modules in this process.
-    HMODULE hMods[1024];
-    DWORD cbNeeded;
-    if (EnumProcessModules(hProcess, hMods, sizeof(hMods), &cbNeeded))
+    // Resize our modules vector to fit all proc modules
+    std::vector<HMODULE> modules(1024);
+    while (true)
     {
-        for (int i = 0; i < (cbNeeded / sizeof(HMODULE)); i++)
-        {
-            std::wstring szModName(MAX_PATH, '\0');
-            // Get the full path to the module's file.
-            // https://docs.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-getmodulefilenameexw
-            if (GetModuleFileNameEx(hProcess, hMods[i], &szModName[0], szModName.size()))
-            {
-                std::wcout << std::format(L"{}\n", szModName);
-                //_tprintf(TEXT("\t%s (0x%08X)\n"), szModName, hMods[i]);
-            }
-        }
+        const DWORD modulesByteSize = modules.size() * sizeof(HMODULE);
+        DWORD bytesNeeded;
+        // https://docs.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-enumprocessmodules
+        if (!EnumProcessModules(hProcess.get(), &modules[0], modulesByteSize, &bytesNeeded))
+            return 1;
+        modules.resize(bytesNeeded / sizeof(HMODULE));
+        if (bytesNeeded <= modulesByteSize)
+            break;
     }
 
-    // Release the handle to the process.
-    CloseHandle(hProcess);
+    for (const auto& procModule : modules)
+    {
+        std::wstring moduleName(MAX_PATH, '\0');
+        // Get the full path to the module's file.
+        // https://docs.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-getmodulefilenameexw
+        if (GetModuleFileNameEx(hProcess.get(), procModule, &moduleName[0], moduleName.size()))
+            std::wcout << std::format(L"{}\n", moduleName);
+    }
 
     return 0;
 }
 
 int AllProcesses()
 {
+    // Get the list of process identifiers.
     DWORD aProcesses[1024];
     DWORD cbNeeded;
-    DWORD cProcesses;
-    unsigned int i;
-
-    // Get the list of process identifiers.
+    // https://docs.microsoft.com/en-us/windows/win32/api/psapi/nf-psapi-enumprocesses
     if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded))
         return 1;
 
     // Calculate how many process identifiers were returned.
-
-    cProcesses = cbNeeded / sizeof(DWORD);
+    DWORD cProcesses = cbNeeded / sizeof(DWORD);
 
     // Print the names of the modules for each process.
-
-    for (i = 0; i < cProcesses; i++)
+    for (int i = 0; i < cProcesses; i++)
     {
         PrintModules(aProcesses[i]);
     }
