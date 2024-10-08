41829de01e83472dd895a1b74bc616455297c8c1
Vasilios Magriplis <etameson@gmail.com>
2024-10-08 21:03:33 +1100

    Minor changes.

--- src/SetPrivilege/main.cpp
+++ src/SetPrivilege/main.cpp
@@ -5,15 +5,15 @@ import std;
 #pragma comment(lib, "advapi32.lib")
 
 bool SetPrivilege(
-    HANDLE hToken,          // access token handle
-    std::wstring_view lpszPrivilege,  // name of privilege to enable/disable
-    bool bEnablePrivilege   // to enable or disable privilege
+    HANDLE token,          // access token handle
+    std::wstring_view privilege,  // name of privilege to enable/disable
+    bool enablePrivilege   // to enable or disable privilege
 )
 {
     LUID luid;
     if (not LookupPrivilegeValue(
         nullptr,            // lookup privilege on local system
-        lpszPrivilege.data(),   // privilege to lookup 
+        privilege.data(),   // privilege to lookup 
         &luid))        // receives LUID of privilege
     {
         std::println("LookupPrivilegeValue error: {}", GetLastError());
@@ -25,14 +25,14 @@ bool SetPrivilege(
         .Privileges = {
             {
                 .Luid = luid, 
-                .Attributes = bEnablePrivilege ? static_cast<DWORD>(SE_PRIVILEGE_ENABLED) : 0
+                .Attributes = enablePrivilege ? static_cast<DWORD>(SE_PRIVILEGE_ENABLED) : 0
             }
         }
     };
 
     // Enable the privilege or disable all privileges.
     if (not AdjustTokenPrivileges(
-        hToken,
+        token,
         false,
         &tp,
         sizeof(TOKEN_PRIVILEGES),
@@ -63,11 +63,23 @@ namespace Privilege
     constexpr std::wstring_view SystemTime = SE_SYSTEMTIME_NAME;
 }
 
-int main()
+HANDLE GetProcessToken()
 {
     HANDLE out = nullptr;
-    OpenProcessToken(GetCurrentProcess(), TOKEN_ALL_ACCESS, &out);
+    if (not OpenProcessToken(GetCurrentProcess(), TOKEN_ALL_ACCESS, &out))
+        throw std::runtime_error(std::format("Failed opening handle {}", GetLastError()));
+    return out;
+}
+
+int main() try
+{
+    HANDLE out = GetProcessToken();
     SetPrivilege(out, Privilege::SystemTime, false);
     return 0;
+}
+catch (const std::exception& ex)
+{
+    std::println("Failed! {}", ex.what());
+    return 1;
 }
 
